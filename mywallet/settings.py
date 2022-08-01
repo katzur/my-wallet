bfc79d9a52f840f83ab643ff03f1abbd88c00ddf
katzur <zurawska.kasia@gmail.com>
2022-08-01 11:58:49 +0000

    Add Bootstrap templates and connect them with CSS and HTML files

--- mywallet/settings.py
+++ mywallet/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'income',
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -133,7 +135,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
